50c76f4d2d3f106cfbde447cec992687c9837ba9
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2015-06-12 19:18:52 +0200

    added scripts

--- lxplusbatchscript.sh
+++ lxplusbatchscript.sh
@@ -2,16 +2,102 @@
 cd /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/CMSSW_5_3_20/src
 #cmsenv
 eval `scramv1 runtime -sh`
-cd /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/CMSSW_5_3_20/src/GeneratorInterface/QPythiaInterface/test/
-nJobs=3
-export nEvents=2000
+
+nPyquen=1
+nQPythia=1
+nPyquenWide=1
+nJewel=1
+nPythiaZ2=1
+nJewelDijet=1
+
+jobNum=0
+
+export nEvents=3
+export PythiaZ2=false
+export QPythia=false
+export Pyquen=false
+export PyquenWide=false
+export JewelDijet=false
+export Jewel=false
+
 i=0
-while [ $i -le $nJobs ];
+while [ $i -lt $nPythiaZ2 ]
 do
-    let "start=i*500"
-    let "end=(i+1)*500"
-    export jobNum=i
-    bsub -R "pool>5000" -M 300000 -q 1nd -J job_${i} < /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/bashScripts/submit.sh
+    export PythiaZ2=true
+    export jobNumber=$jobNum
+
+    #   bash /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/bashScripts/submit.sh
+    bsub -R "pool>5000" -M 300000 -q 1nd -J job_${jobNum} < /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/bashScripts/submit.sh
     let "i++"
+    let "jobNum++"
 done
-echo "submit all jobs!"
+export PythiaZ2=false
+
+
+i=0
+while [ $i -lt $nQPythia ]
+do
+    export QPythia=true
+    export jobNumber=$jobNum
+
+    #bash /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/bashScripts/submit.sh 
+ bsub -R "pool>5000" -M 300000 -q 1nd -J job_${jobNum} < /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/bashScripts/submit.sh    
+    let "i++"
+    let "jobNum++"
+done
+export QPythia=false
+
+i=0
+while [ $i -lt $nPyquen ]
+do
+    export Pyquen=true
+    export jobNumber=$jobNum   
+ #      bash /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/bashScripts/submit.sh
+bsub -R "pool>5000" -M 300000 -q 1nd -J job_${jobNum} < /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/bashScripts/submit.sh
+    let "i++"
+    let "jobNum++"
+done
+export Pyquen=false
+
+
+i=0
+while [ $i -lt $nPyquenWide ]
+do
+    export PyquenWide=true
+    export jobNumber=$jobNum
+
+  #     bash /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/bashScripts/submit.sh  
+  bsub -R "pool>5000" -M 300000 -q 1nd -J job_${jobNum} < /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/bashScripts/submit.sh
+    let "i++"
+    let "jobNum++"
+done
+export PyquenWide=false
+
+
+i=0
+while [ $i -lt $nJewelDijet ]
+do
+    export JewelDijet=true
+    export jobNumber=$jobNum
+
+   #    bash /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/bashScripts/submit.sh    
+bsub -R "pool>5000" -M 300000 -q 1nd -J job_${jobNum} < /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/bashScripts/submit.sh
+    let "i++"
+    let "jobNum++"
+done
+export JewelDijet=false
+
+i=0
+while [ $i -lt $nJewel ]
+do
+    export Jewel=true
+    export jobNumber=$jobNum
+     
+ #      bash /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/bashScripts/submit.sh
+  bsub -R "pool>5000" -M 300000 -q 1nd -J job_${jobNum} < /afs/cern.ch/work/i/ihuntisa/WORK/RUTGERS/bashScripts/submit.sh
+    let "i++"
+    let "jobNum++"
+done
+export Jewel=false
+
+echo "submitted all jobs!"
